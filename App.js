e11c52a8b85673a4cd205a49d03ce441b72dbbd0
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-08 15:55:47 -0700

    Add test code for iOS

--- App.js
+++ App.js
@@ -1,20 +1,191 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {
+  requestAuthorization,
+  getDailyStepCountSamples,
+  getStepCount,
+  getAuthStatus,
+  getDistance,
+  getDailyDistanceWalkingRunningSamples,
+  getActiveEnergyBurned,
+  getDailyActiveEnergyBurnedSamples,
+} from './src/AppleHealthKit';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [authStatus, setAuthStatus] = useState({});
+  const [todayStep, setTodayStep] = useState(0);
+  const [sevenDaysStep, setSevenDaysStep] = useState(0);
+  const [todayDistance, setTodayDistance] = useState(0);
+  const [sevenDaysDistance, setSevenDaysDistance] = useState(0);
+  const [todayEnergyBarned, setTodayEnergyBarned] = useState(0);
+  const [sevenDaysEnergyBarned, setSevenDaysEnergyBarned] = useState(0);
+
+  useEffect(() => {
+    requestAuthorization((error) => {
+      if (error) {
+        console.log('[ERROR] Cannot grant permissions!');
+        return;
+      }
+
+      getDailyStepCountSamples((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setSevenDaysStep(result);
+      });
+
+      getStepCount((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setTodayStep(result);
+      });
+
+      getDistance((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setTodayDistance(result);
+      });
+
+      getDailyDistanceWalkingRunningSamples((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setSevenDaysDistance(result);
+      });
+
+      getActiveEnergyBurned((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setTodayEnergyBarned(result);
+      });
+
+      getDailyActiveEnergyBurnedSamples((err, result) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setSevenDaysEnergyBarned(result);
+      });
+    });
+  }, [authStatus]);
+
+  const handlePressGetAuthStatus = () => {
+    getAuthStatus((err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      setAuthStatus(result);
+    });
+  };
+
+  if ((Platform.OS = 'ios')) {
+    return (
+      <>
+        <StatusBar barStyle='dark-content' />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior='automatic'
+            style={styles.scrollView}
+          >
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>
+                  React Native Test app
+                </Text>
+                <Text>This is iOS</Text>
+                <Text onPress={handlePressGetAuthStatus}>
+                  Press me to get Auth Status
+                </Text>
+                <Text style={styles.sectionDescription}>
+                  {JSON.stringify(authStatus, null, 2)}
+                </Text>
+
+                <Text style={styles.sectionDescription}>
+                  Today's steps: {todayStep}
+                </Text>
+                <Text style={styles.sectionDescription}>
+                  Past 7 days' steps: {JSON.stringify(sevenDaysStep)}
+                </Text>
+
+                <Text style={styles.sectionDescription}>
+                  Today's distance: {todayDistance}
+                </Text>
+                <Text style={styles.sectionDescription}>
+                  Past 7 days' distance:
+                  {JSON.stringify(sevenDaysDistance)}
+                </Text>
+
+                <Text style={styles.sectionDescription}>
+                  Today's energy barned: {JSON.stringify(todayEnergyBarned)}
+                </Text>
+                <Text style={styles.sectionDescription}>
+                  Past 7 energy barned:
+                  {JSON.stringify(sevenDaysEnergyBarned)}
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  } else {
+    return <Text>This is not iOS</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
---
+++ src/AppleHealthKit.js
@@ -0,0 +1,222 @@
+import AppleHealthKit from 'react-native-health';
+
+/* Permission options */
+const params = AppleHealthKit.Constants.Permissions;
+const permissions = {
+  permissions: {
+    read: [
+      params.Steps,
+      params.ActiveEnergyBurned,
+      params.DistanceWalkingRunning,
+    ],
+    write: [],
+  },
+};
+
+// initialize permissions
+export const requestAuthorization = (callback) => {
+  AppleHealthKit.initHealthKit(permissions, (error) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    callback(null);
+  });
+};
+
+export const getAuthStatus = (callback) => {
+  AppleHealthKit.getAuthStatus(permissions, (err, result) => {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    callback(null, result);
+  });
+};
+
+// GET health info
+// date constant
+const today = new Date();
+const sevenDaysAgo = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7
+);
+today.setHours(0, 0, 0, 0);
+
+// Today's current step count
+export const getStepCount = (callback) => {
+  let options = {
+    includeManuallyAdded: true, // for iOS simulator, inlude data manually added
+  };
+
+  AppleHealthKit.getStepCount(options, function (err, result) {
+    if (err) {
+      console.error('Error in getStepCount:', err);
+      callback(err, null);
+      return;
+    }
+
+    // console.log('Current daily step(getStepCount):', result.value);
+    callback(null, result.value);
+  });
+};
+
+// Daily step count for the past 7 days
+export const getDailyStepCountSamples = (callback) => {
+  // Past 7 days to today
+  let options = {
+    startDate: sevenDaysAgo.toISOString(),
+    endDate: today.toISOString(),
+  };
+
+  AppleHealthKit.getDailyStepCountSamples(options, function (err, results) {
+    if (err) {
+      console.error('Error in getDailyStepCountSamples: ', err);
+      callback(err, null);
+      return;
+    }
+    const data = results;
+    const dailySteps = {};
+
+    // calculate step count on each day
+    data.forEach((entry) => {
+      const startDate = entry.startDate.split('T')[0];
+      const value = entry.value;
+
+      if (dailySteps[startDate]) {
+        dailySteps[startDate] += value; // update step count
+      } else {
+        dailySteps[startDate] = value; // add new step count
+      }
+    });
+
+    // console.log('getDailyStepCountSamples: ', dailySteps);
+    callback(null, dailySteps);
+  });
+};
+
+// Today's current distance
+export const getDistance = (callback) => {
+  let options = {
+    includeManuallyAdded: true, // for iOS simulator, inlude data manually added
+  };
+
+  AppleHealthKit.getDistanceWalkingRunning(options, function (err, result) {
+    if (err) {
+      console.error('Error in getStepCount:', err);
+      callback(err, null);
+      return;
+    }
+
+    callback(null, result.value);
+    // console.log('getDistance: ', result.value);
+  });
+};
+
+// Distance for the past 7 days
+export const getDailyDistanceWalkingRunningSamples = (callback) => {
+  // Past 7 days to today
+  let options = {
+    startDate: sevenDaysAgo.toISOString(),
+    endDate: today.toISOString(),
+  };
+
+  AppleHealthKit.getDailyDistanceWalkingRunningSamples(
+    options,
+    function (err, results) {
+      if (err) {
+        console.error('Error in getDailyDistanceWalkingRunningSamples: ', err);
+        callback(err, null);
+        return;
+      }
+      const data = results;
+      const dailyDistances = {};
+
+      // calculate distance on each day
+      data.forEach((entry) => {
+        const startDate = entry.startDate.split('T')[0];
+        const value = entry.value;
+
+        if (dailyDistances[startDate]) {
+          dailyDistances[startDate] += value;
+        } else {
+          dailyDistances[startDate] = value;
+        }
+      });
+
+      //   console.log('getDailyDistanceWalkingRunningSamples: ', dailyDistances);
+      callback(null, dailyDistances);
+    }
+  );
+};
+
+// Today's current Active Energy Burned
+export const getActiveEnergyBurned = (callback) => {
+  let options = {
+    startDate: today.toISOString(), 
+  };
+
+  AppleHealthKit.getActiveEnergyBurned(options, function (err, results) {
+    if (err) {
+        console.error('Error in getDailyActiveEnergyBurnedSamples: ', err);
+        callback(err, null);
+        return;
+      }
+      const data = results;
+      const dailyEnergyBarned = {};
+  
+      // calculate distance on each day
+      data.forEach((entry) => {
+        const startDate = entry.startDate.split('T')[0];
+        const value = entry.value;
+  
+        if (dailyEnergyBarned[startDate]) {
+          dailyEnergyBarned[startDate] += value;
+        } else {
+          dailyEnergyBarned[startDate] = value;
+        }
+      });
+  
+      console.log('getActiveEnergyBurned: ', dailyEnergyBarned);
+      callback(null, dailyEnergyBarned);
+    });
+};
+
+// Active Energy Burned for the past 7 days
+export const getDailyActiveEnergyBurnedSamples = (callback) => {
+  // Past 7 days to today
+  let options = {
+    startDate: sevenDaysAgo.toISOString(),
+    endDate: today.toISOString(),
+  };
+
+  AppleHealthKit.getActiveEnergyBurned(options, function (err, results) {
+    if (err) {
+      console.error('Error in getDailyActiveEnergyBurnedSamples: ', err);
+      callback(err, null);
+      return;
+    }
+    const data = results;
+    const dailyEnergyBarned = {};
+
+    // calculate distance on each day
+    data.forEach((entry) => {
+      const startDate = entry.startDate.split('T')[0];
+      const value = entry.value;
+
+      if (dailyEnergyBarned[startDate]) {
+        dailyEnergyBarned[startDate] += value;
+      } else {
+        dailyEnergyBarned[startDate] = value;
+      }
+    });
+
+    console.log('getDailyActiveEnergyBurnedSamples: ', dailyEnergyBarned);
+    callback(null, dailyEnergyBarned);
+  });
+};
+
+
+
