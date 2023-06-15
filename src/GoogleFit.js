e58ba72d3af9ea588a53fe5c08b6b4ab34d1cdcd
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-14 18:47:04 -0700

    Shorten Google fit API

--- src/GoogleFit.js
+++ src/GoogleFit.js
@@ -2,25 +2,12 @@ import GoogleFit, { Scopes } from 'react-native-google-fit';
 
 /* Permission options */
 const options = {
-  scopes: [
-    Scopes.FITNESS_ACTIVITY_READ,
-    Scopes.FITNESS_ACTIVITY_WRITE,
-    // Scopes.FITNESS_BODY_READ,
-    // Scopes.FITNESS_BODY_WRITE,
-  ],
+  scopes: [Scopes.FITNESS_ACTIVITY_READ, Scopes.FITNESS_LOCATION_READ],
 };
 
 // initialize permissions
 export async function authorizeFit() {
   try {
-    const options = {
-      scopes: [
-        Scopes.FITNESS_ACTIVITY_READ,
-        Scopes.FITNESS_ACTIVITY_WRITE,
-        Scopes.FITNESS_BODY_READ,
-        Scopes.FITNESS_BODY_WRITE,
-      ],
-    };
     const authResult = await GoogleFit.authorize(options);
     if (authResult.success) {
       return authResult;
@@ -42,192 +29,41 @@ const sevenDaysAgo = new Date(
 );
 today.setHours(0, 0, 0, 0);
 
+const opt = {
+  startDate: today.toISOString(),
+  endDate: new Date().toISOString(),
+};
+
 // Today's current step count
 export const getStepCount = async () => {
-  const opt = {
-    startDate: today.toISOString(),
-    endDate: new Date().toISOString(),
-    bucketInterval: 1,
-  };
-
   try {
-    const authResult = await authorizeFit(); // authorizeFit 関数を呼び出す
+    const authResult = await authorizeFit();
     const res = await GoogleFit.getDailyStepCountSamples(opt);
-    console.log(res);
-    return res[1].steps[0].value;
+    console.log('steps: ', res);
+    return res[1].steps[0];
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
 export const getDistance = async () => {
-  const opt = {
-    startDate: today.toISOString(),
-    endDate: new Date().toISOString(),
-    bucketInterval: 1,
-  };
-
   try {
-    const authResult = await authorizeFit(); // authorizeFit 関数を呼び出す
+    const authResult = await authorizeFit();
     const res = await GoogleFit.getDailyDistanceSamples(opt);
-    console.log(res);
-    return res[1].steps[0].value;
+    console.log('distance: ', res);
+    return res[0];
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
-// // Daily step count for the past 7 days
-// export const getDailyStepCountSamples = (callback) => {
-//   // Past 7 days to today
-//   let options = {
-//     startDate: sevenDaysAgo.toISOString(),
-//     endDate: today.toISOString(),
-//   };
-
-//   AppleHealthKit.getDailyStepCountSamples(options, function (err, results) {
-//     if (err) {
-//       console.error('Error in getDailyStepCountSamples: ', err);
-//       callback(err, null);
-//       return;
-//     }
-//     const data = results;
-//     const dailySteps = {};
-
-//     // calculate step count on each day
-//     data.forEach((entry) => {
-//       const startDate = entry.startDate.split('T')[0];
-//       const value = entry.value;
-
-//       if (dailySteps[startDate]) {
-//         dailySteps[startDate] += value; // update step count
-//       } else {
-//         dailySteps[startDate] = value; // add new step count
-//       }
-//     });
-
-//     // console.log('getDailyStepCountSamples: ', dailySteps);
-//     callback(null, dailySteps);
-//   });
-// };
-
-// // Today's current distance
-// export const getDistance = (callback) => {
-//   let options = {
-//     includeManuallyAdded: true, // for iOS simulator, inlude data manually added
-//   };
-
-//   AppleHealthKit.getDistanceWalkingRunning(options, function (err, result) {
-//     if (err) {
-//       console.error('Error in getStepCount:', err);
-//       callback(err, null);
-//       return;
-//     }
-
-//     callback(null, result.value);
-//     // console.log('getDistance: ', result.value);
-//   });
-// };
-
-// // Distance for the past 7 days
-// export const getDailyDistanceWalkingRunningSamples = (callback) => {
-//   // Past 7 days to today
-//   let options = {
-//     startDate: sevenDaysAgo.toISOString(),
-//     endDate: today.toISOString(),
-//   };
-
-//   AppleHealthKit.getDailyDistanceWalkingRunningSamples(
-//     options,
-//     function (err, results) {
-//       if (err) {
-//         console.error('Error in getDailyDistanceWalkingRunningSamples: ', err);
-//         callback(err, null);
-//         return;
-//       }
-//       const data = results;
-//       const dailyDistances = {};
-
-//       // calculate distance on each day
-//       data.forEach((entry) => {
-//         const startDate = entry.startDate.split('T')[0];
-//         const value = entry.value;
-
-//         if (dailyDistances[startDate]) {
-//           dailyDistances[startDate] += value;
-//         } else {
-//           dailyDistances[startDate] = value;
-//         }
-//       });
-
-//       //   console.log('getDailyDistanceWalkingRunningSamples: ', dailyDistances);
-//       callback(null, dailyDistances);
-//     }
-//   );
-// };
-
-// // Today's current Active Energy Burned
-// export const getActiveEnergyBurned = (callback) => {
-//   let options = {
-//     startDate: today.toISOString(),
-//   };
-
-//   AppleHealthKit.getActiveEnergyBurned(options, function (err, results) {
-//     if (err) {
-//       console.error('Error in getDailyActiveEnergyBurnedSamples: ', err);
-//       callback(err, null);
-//       return;
-//     }
-//     const data = results;
-//     const dailyEnergyBarned = {};
-
-//     // calculate distance on each day
-//     data.forEach((entry) => {
-//       const startDate = entry.startDate.split('T')[0];
-//       const value = entry.value;
-
-//       if (dailyEnergyBarned[startDate]) {
-//         dailyEnergyBarned[startDate] += value;
-//       } else {
-//         dailyEnergyBarned[startDate] = value;
-//       }
-//     });
-
-//     // console.log('getActiveEnergyBurned: ', dailyEnergyBarned);
-//     callback(null, dailyEnergyBarned);
-//   });
-// };
-
-// // Active Energy Burned for the past 7 days
-// export const getDailyActiveEnergyBurnedSamples = (callback) => {
-//   // Past 7 days to today
-//   let options = {
-//     startDate: sevenDaysAgo.toISOString(),
-//     endDate: today.toISOString(),
-//   };
-
-//   AppleHealthKit.getActiveEnergyBurned(options, function (err, results) {
-//     if (err) {
-//       console.error('Error in getDailyActiveEnergyBurnedSamples: ', err);
-//       callback(err, null);
-//       return;
-//     }
-//     const data = results;
-//     const dailyEnergyBarned = {};
-
-//     // calculate distance on each day
-//     data.forEach((entry) => {
-//       const startDate = entry.startDate.split('T')[0];
-//       const value = entry.value;
-
-//       if (dailyEnergyBarned[startDate]) {
-//         dailyEnergyBarned[startDate] += value;
-//       } else {
-//         dailyEnergyBarned[startDate] = value;
-//       }
-//     });
-
-//     // console.log('getDailyActiveEnergyBurnedSamples: ', dailyEnergyBarned);
-//     callback(null, dailyEnergyBarned);
-//   });
-// };
+export const getCalories = async () => {
+  try {
+    const authResult = await authorizeFit();
+    const res = await GoogleFit.getDailyCalorieSamples(opt);
+    console.log('calories: ', res);
+    return res[0];
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
--- src/Screen.android.js
+++ src/Screen.android.js
@@ -2,37 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import {
-  getStepCount,
-} from './GoogleFit';
+import { getStepCount, getDistance, getCalories } from './GoogleFit';
 
 export default function Screen() {
   const [authStatus, setAuthStatus] = useState(false);
-  const [todayStep, setTodayStep] = useState(0);
-  const [sevenDaysStep, setSevenDaysStep] = useState(0);
-  const [todayDistance, setTodayDistance] = useState(0);
+  const [steps, setSteps] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [calories, setCalories] = useState(0);
 
   useEffect(() => {
     async function test() {
       try {
-        const steps = await getStepCount();
-        setTodayStep(steps);
-        const distance = await getStepCount();
-        setTodayStep(steps);
-        const calories = await getStepCount();
-        setTodayStep(steps);
-
+        const stepResult = await getStepCount();
+        setSteps(stepResult);
+        const distanceResult = await getDistance();
+        setDistance(distanceResult);
+        const caloriesResult = await getCalories();
+        setCalories(caloriesResult);
       } catch (error) {
         console.log('Error :', error.message);
       }
     }
     test();
-    // ...
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>stepData: {todayStep}</Text>
+      <Text>step: {steps.value}</Text>
+      <Text>distance: {distance.distance}</Text>
+      <Text>calories: {calories.calorie}</Text>
       <StatusBar style='auto' />
     </View>
   );
